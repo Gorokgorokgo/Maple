ee21691c01cfd90cfa7a0e1ab988a8e9bf34f47f
Park_Yong_Jun <dydwnsl02@naver.com>
2025-05-19 02:49:07 +0900

    fix : swagger 수정

--- auth-server/src/auth/auth.controller.ts
+++ auth-server/src/auth/auth.controller.ts
@@ -14,7 +14,8 @@ export class AuthController {
   ) {}
   
   @Post('register')
-  @ApiOperation({ summary: '회원가입', description: '새로운 유저를 등록합니다.' })
+  @ApiOperation({ summary: '회원가입', description: 'ID : 영문 + 숫자, 4자 이상 20자까지<br/>' +
+    'PASSWORD : 소문자, 대문자, 숫자, 특수문자를 모두 포함하여 8자 이상 15자리' })
   @ApiResponse({ status: 201, description: '회원가입 성공', type: User })
   @ApiResponse({ status: 409, description: 'ID 또는 닉네임 중복' })
   async register(@Body() createUserDto: CreateUserDto): Promise<User> {
@@ -22,7 +23,7 @@ export class AuthController {
   }
 
   @Post('login')
-  @ApiOperation({ summary: '로그인', description: 'ID : 영문 + 숫자, 4자 이상 20자까지  / PASSWORD : 소문자, 대문자, 숫자, 특수문자를 모두 포함하여 8자 이상 15자리' })
+  @ApiOperation({ summary: '로그인', description: 'ID + PW 조합으로 JWT를 발급합니다.' })
   @ApiResponse({ status: 200, description: '로그인 성공, JWT 반환' })
   @ApiResponse({ status: 401, description: 'ID 또는 PW 불일치' })
   async login(@Body() loginDto: LoginDto) {
